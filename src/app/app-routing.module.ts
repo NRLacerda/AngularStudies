305ca1f1dc7d73aca3ba12fbf2c4d111cd1a4ae5
NRLacerda <nrochalacerda@gmail.com>
2022-10-06 11:05:01 -0300

    att

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
+import { TesthashComponent } from './testhash/testhash.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
 
-const routes: Routes = [{ path: 'heroes', component: HeroesComponent }];
+const routes: Routes = 
+[
+  /*canActivate: []* Só abre a rota se for
+  suprido as necessidades do activate, geralmente um Auth0 */
+  { path: 'heroes', component: HeroesComponent,},
+  {path:'teste/teste2', component:TesthashComponent}
+];
+
 
 @NgModule({
   declarations: [],
