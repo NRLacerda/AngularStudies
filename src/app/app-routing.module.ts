32d02745789095587bc17b45972e7d2b8f0b86cc
NRLacerda <nrochalacerda@gmail.com>
2022-10-17 08:48:55 -0300

    att

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,21 @@
-import { NotfoundComponent } from './notfound/notfound.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { TesthashComponent } from './testhash/testhash.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
-const routes: Routes =
-[
+const routes: Routes = [
   /*canActivate: []* Só abre a rota se for
   suprido as necessidades do activate, geralmente um Auth0 */
-  { path: 'heroes', component: HeroesComponent,},
-  {path:'teste/teste2', component:TesthashComponent},
-  { path: '**', pathMatch: 'full', 
-        component: NotfoundComponent },
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'teste/teste2', component: TesthashComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'detail/:id', component: HeroDetailComponent },
+  
+    // path de not found { path: '**', pathMatch: 'full', component: NotfoundComponent },
 ];
-
 
 @NgModule({
   declarations: [],
--- src/app/testhash/testhash.component.ts
+++ src/app/testhash/testhash.component.ts
@@ -9,13 +9,15 @@ import * as sha256 from 'fast-sha256';
 export class TesthashComponent implements OnInit {
   constructor() {}
 
-  ngOnInit(): void {/*
-    function hash() {
-      var crypto = require('crypto');
-      var name = '@n1Mseguranza';
-      var hash = crypto.createHash('md5').update(name).digest('hex');
-      console.log(hash); // 9b74c9897bac770ffc029102a200c5de
-    */
+  ngOnInit(): void {
    }
+  /*
+  hash() {
+    var crypto = require('crypto');
+    var name = '@n1Mseguranza';
+    var hash = crypto.createHash('md5').update(name).digest('hex');
+    console.log(hash); // 9b74c9897bac770ffc029102a200c5de
+  }
+  */
   }
 
