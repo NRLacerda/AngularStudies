5349251fbf73147d787002b727950e94e56a7803
NRLacerda <nrochalacerda@gmail.com>
2022-11-18 08:31:57 -0300

    tabela

--- src/app/tabela/tabela.component.ts
+++ src/app/tabela/tabela.component.ts
@@ -1,17 +1,48 @@
 import { Component, OnInit } from '@angular/core';
-import Chart from 'chart.js/auto';
+
+import {Chart} from 'chart.js';
 
 
 @Component({
-  selector: 'app-tabela',
+  selector: 'tabela',
   templateUrl: './tabela.component.html',
   styleUrls: ['./tabela.component.css']
 })
 export class TabelaComponent implements OnInit {
-
+  public chart: any;
   constructor() { }
 
   ngOnInit(): void {
+    this.createChart();
+  }
+  createChart(){
+  
+    this.chart = new Chart("MyChart", {
+      type: 'line', //this denotes tha type of chart
+
+      data: {// values on X-Axis
+        labels: ['2022-05-10', '2022-05-11', '2022-05-12','2022-05-13',
+								 '2022-05-14', '2022-05-15', '2022-05-16','2022-05-17', ], 
+	       datasets: [
+          {
+            label: "Sales",
+            data: ['467','576', '572', '79', '92',
+								 '574', '573', '576'],
+            backgroundColor: 'blue'
+          },
+          {
+            label: "Profit",
+            data: ['542', '542', '536', '327', '17',
+									 '0.00', '538', '541'],
+            backgroundColor: 'white'
+          }  
+        ]
+      },
+      options: {
+        aspectRatio:2.5
+      }
+      
+    });
   }
 
 }
