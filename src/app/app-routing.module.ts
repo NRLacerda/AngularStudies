1eb388720ea87a17bd69d439c0e1b59eb1a9de9f
NRLacerda <nrochalacerda@gmail.com>
2022-11-18 14:40:57 -0300

    att graf venaa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { TabelaComponent } from './tabela/tabela.component';
 
 const routes: Routes = [
   /*canActivate: []* Só abre a rota se for
@@ -12,7 +13,8 @@ const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'detail/:id', component: HeroDetailComponent },
-  { path: 'todo', component:ListItemComponent}
+  { path: 'todo', component:ListItemComponent},
+  {path: 'vendas', component:TabelaComponent}
     // path de not found { path: '**', pathMatch: 'full', component: NotfoundComponent },
 ];
 
--- src/app/tabela/tabela.component.ts
+++ src/app/tabela/tabela.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
-import {Chart} from 'chart.js';
-
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'tabela',
@@ -28,18 +26,18 @@ export class TabelaComponent implements OnInit {
             label: "Sales",
             data: ['467','576', '572', '79', '92',
 								 '574', '573', '576'],
-            backgroundColor: 'blue'
+            backgroundColor: 'red'
           },
           {
             label: "Profit",
             data: ['542', '542', '536', '327', '17',
 									 '0.00', '538', '541'],
-            backgroundColor: 'white'
+            backgroundColor: 'green'
           }  
         ]
       },
       options: {
-        aspectRatio:2.5
+        aspectRatio:3
       }
       
     });
